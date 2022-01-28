7926c3867934610f588e9858be453321ae998bbc
jerzmerner <rickjburner@gmail.com>
2022-01-28 11:55:01 -0500

    Module 4

--- stock_data.py
+++ stock_data.py
@@ -18,19 +18,79 @@ from stock_class import Stock, DailyData
 
 # Create the SQLite database
 def create_database():
-    pass #Replace with code to create database
+    stockDB = "stocks.db"
+    conn = sqlite3.connect(stockDB)
+    cur = conn.cursor()
+    createStockTableCmd = """CREATE TABLE IF NOT EXISTS stocks (
+                            symbol TEXT NOT NULL PRIMARY KEY,
+                            name TEXT,
+                            shares REAL
+                        );"""
+    createDailyDataTableCmd = """CREATE TABLE IF NOT EXISTS dailyData (
+                                symbol TEXT NOT NULL,
+                                date TEXT NOT NULL,
+                                price REAL NOT NULL,
+                                volume REAL NOT NULL,
+                                PRIMARY KEY (symbol, date)
+                        );"""   
+    cur.execute(createStockTableCmd)
+    cur.execute(createDailyDataTableCmd)
 
 # Save stocks and daily data into database
 def save_stock_data(stock_list):
-    clear_screen()
-    print("*** This Module Under Construction ***")
-    _ = input("*** Press Enter to Continue ***")
+    #clear_screen()
+    stockDB = "stocks.db"
+    conn = sqlite3.connect(stockDB)
+    cur = conn.cursor()
+    insertStockCmd = """INSERT INTO stocks
+                            (symbol, name, shares)
+                            VALUES
+                            (?, ?, ?); """
+    insertDailyDataCmd = """INSERT INTO dailyData
+                                    (symbol, date, price, volume)
+                                    VALUES
+                                    (?, ?, ?, ?);"""
+    for stock in stock_list:
+        insertValues = (stock.symbol, stock.name, stock.shares)
+        try:
+            cur.execute(insertStockCmd, insertValues)
+            cur.execute("COMMIT;")
+        except:
+            pass
+        for daily_data in stock.DataList: 
+            insertValues = (stock.symbol,daily_data.date.strftime("%m/%d/%y"),daily_data.close,daily_data.volume)
+            try:
+                cur.execute(insertDailyDataCmd, insertValues)
+                cur.execute("COMMIT;")
+            except:
+                pass
+
     
 # Load stocks and daily data from database
 def load_stock_data(stock_list):
-    clear_screen()
-    print("*** This Module Under Construction ***")
-    _ = input("*** Press Enter to Continue ***")
+    #clear_screen()
+    stock_list.clear()
+    stockDB = "stocks.db"
+    conn = sqlite3.connect(stockDB)
+    stockCur = conn.cursor()
+    stockSelectCmd = """SELECT symbol, name, shares
+                    FROM stocks; """
+    stockCur.execute(stockSelectCmd)
+    stockRows = stockCur.fetchall()
+    for row in stockRows:
+        new_stock = Stock(row[0],row[1],row[2])
+        dailyDataCur = conn.cursor()
+        dailyDataCmd = """SELECT date, price, volume
+                        FROM dailyData
+                        WHERE symbol=?; """
+        selectValue = (new_stock.symbol)
+        dailyDataCur.execute(dailyDataCmd,(selectValue,))
+        dailyDataRows = dailyDataCur.fetchall()
+        for dailyRow in dailyDataRows:
+            daily_data = DailyData(datetime.strptime(dailyRow[0],"%m/%d/%y"),float(dailyRow[1]),float(dailyRow[2]))
+            new_stock.add_data(daily_data)
+        stock_list.append(new_stock)
+    sortDailyData(stock_list)
 
 # Get stock price history from web using Web Scraping
 def retrieve_stock_web(dateStart,dateEnd,stock_list):
